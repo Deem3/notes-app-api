b534cdbb5397764ec72a7157e263d604bb8329f4
Deem3 <erdemee.t@gmail.com>
2024-01-01 02:55:46 +0800

    fix: add CORS configuration to enable cross-origin requests

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ credentials: true, origin: true });
   const config = new DocumentBuilder()
     .setTitle('TemplateApi')
     .setDescription('Template api for nestjs')
